d07fea07b6f5cc5343580a2e180a6f241ce7bd93
Lev Goukassian <Leogouk@gmail.com>
2025-07-22 11:53:19 -0700

    Update __init__.py

--- src/goukassian/__init__.py
+++ src/goukassian/__init__.py
@@ -1,1 +1,28 @@
+"""
+Goukassian Framework - Ternary Logic for Intelligent Decision-Making
+Created by Lev Goukassian (ORCID: 0009-0006-5966-1243)
+Contact: leogouk@gmail.com
 
+"The world is not binary. And the future will not be either."
+"""
+
+from .core import (
+    TernaryState,
+    TernaryValue, 
+    TernaryResult,
+    TernaryLogicEngine,
+    TernaryDecisionEngine
+)
+
+__version__ = "1.0.0"
+__author__ = "Lev Goukassian"
+__email__ = "leogouk@gmail.com"
+__orcid__ = "0009-0006-5966-1243"
+
+__all__ = [
+    "TernaryState",
+    "TernaryValue",
+    "TernaryResult", 
+    "TernaryLogicEngine",
+    "TernaryDecisionEngine"
+]
